724891c33f0f0fb80bdf05ff9ef4b9e7d309ef8a
MathieuArnaud <mathieu.arnaud43@gmail.com>
2020-08-03 14:28:17 +0200

    carouselTest

--- carousel.php
+++ carousel.php
@@ -3,43 +3,52 @@
 echo '
 <script src="carousel.js"></script>
 <link rel="stylesheet" type="text/css" href="carousel.css">
+
 <div id="carousel">
-    <div id="c1">
+    <div id="left">
+        <img src="Left.png" class="imgArrow">
+    </div>
+    <div id="carouselContainer">
+    <div id="c1" class="cCenter">
         <img src="cyber1.png" class="imgCar">
         <p class="describCar">
-        ce genre de describe
+        ce genre de describe 1
         </p>
     </div>
-    <div id="c2">
+    <div id="c2" class="cRight">
         <img src="cyber2.png" class="imgCar">
         <p class="describCar">
-        ce genre de describe
+        ce genre de describe 2
         </p>
     </div>
     <div id="c3">
         <img src="cyber3.png" class="imgCar">
         <p class="describCar">
-        ce genre de describe
+        ce genre de describe 3
         </p>        
     </div>
     <div id="c4">
         <img src="cyber4.png" class="imgCar">
         <p class="describCar">
-        ce genre de describe
+        ce genre de describe 4
         </p>
     </div>
     <div id="c5">
         <img src="cyber5.png" class="imgCar">
         <p class="describCar">
-        ce genre de describe
+        ce genre de describe 5
         </p>
     </div>
-    <div id="c6">
+    <div id="c6" class="cLeft">
         <img src="cyber6.png" class="imgCar">
         <p class="describCar">
-        ce genre de describe
+        ce genre de describe 6
         </p>
+        </div>
     </div>
+    <div id="right">
+    <img src="Right.png" class="imgArrow">
+</div>
 
 
 
